352bc8452b5d247018eab8a38140b201bc6cfb81
uzumbek <uzumapps@gmail.com>
2024-11-08 17:23:12 +0100

    listen on localhost

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ const server = createServer((req, res) => {
   res.end('Hello World');
 });
 server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://`);
 });
